6521f8f0b809a7257e981b12df4545b0dc703102
knTkd <gloel.loe2@gmail.com>
2021-03-08 13:41:54 +0900

    lift up State (Board -> Game)

--- src/index.js
+++ src/index.js
@@ -2,21 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Reset(props) {
-  return (
-      <button className="reset" onClick={props.onClick}>
-      ResetButtonnnnn
-    </button>    
-  );
-}
-
-function Square(props) {
-  return (
-      <button className="square" onClick={props.onClick}>
-        {props.value}
-      </button>
-  );
-}
 
 function calculateWinner(squares) {
   const lines = [
@@ -37,60 +22,44 @@ function calculateWinner(squares) {
   return null;
 }
 
+function Reset(props) {
+  return (
+      <button className="reset" onClick={props.onClick}>
+      ResetButtonnnnn
+    </button>    
+  );
+}
+
+function Square(props) {
+  return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+  );
+}
+
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      turnCount: 0,
-    };
-  }
 
-  handleClick(i) {
-    const newsquares = this.state.squares.slice();
-    if (newsquares[i] != null || calculateWinner(newsquares))  return;
-//    let ch = 'X';
-    //    if (this.state.turnCount % 2 == 0)  ch = 'O';
-    //    newsquares[i] = ch;
-    newsquares[i] = (this.state.turnCount % 2 == 0 ? 'O' : 'X');
-    this.setState({
-      squares: newsquares,
-      turnCount: this.state.turnCount + 1,
-    });
-  }
-
-  handleReset() {
-    this.setState({squares: Array(9).fill(null), turnCount: 0});
+  renderReset() {
+    return (
+      <Reset
+      onClick={ () => this.props.resetClick() }
+      />
+    );
   }
 
   renderSquare(i) {
     return (
 	<Square 
-      value={this.state.squares[i]}
-      onClick={ () => this.handleClick(i) }
+      value={this.props.squares[i]}
+      onClick={ () => this.props.squareClick(i) }
 	/>
     );
   }
 
-  renderReset() {
-    return (
-      <Reset
-      onClick={ () => this.handleReset() }
-      />
-    );
-  }
-
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner : ' + winner + ' !';
-    } else {
-      status = 'Next Player: ' + (this.state.turnCount % 2 == 0 ? 'O' : 'X');
-    }
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -115,14 +84,62 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+	squares: Array(9).fill(null),
+      }],
+      turnCount: 0
+    };
+  }
+
+  handleReset() {
+    this.setState({
+      history: [{
+	squares: Array(9).fill(null),
+      }],
+      turnCount: 0
+    });
+  }
+
+  handleSquare(i) {
+    // l ... mean local
+    const lhistory = this.state.history;
+    const lcurrent = lhistory[lhistory.length - 1];
+    const lsquares = lcurrent.squares.slice();
+
+    if (lsquares[i] || calculateWinner(lsquares))  return;
+    lsquares[i] = this.state.turnCount % 2 == 0 ? 'O' : 'X';
+    this.setState({
+      history: lhistory.concat([{
+	squares: lsquares,
+      }]),
+      turnCount: this.state.turnCount + 1,
+    });
+  }
+
+
   render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+    let status;
+    if (winner)  status = 'WINNER : ' + winner + ' !';
+    else  status = 'Next Player : ' + (this.state.turnCount % 2 == 0 ? 'O' : 'X');
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board 
+      squares={current.squares}
+      squareClick={(i) => this.handleSquare(i)}
+      resetClick={() => this.handleReset()}
+	/>
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{ status }</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
